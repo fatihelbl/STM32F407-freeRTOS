8441ac2312139aefa1b4869a077daa24c8398364
Fatih Elibol <elibollfatih@gmail.com>
2025-06-30 16:36:34 +0300

    added task

--- freeRTOS Project/Core/Src/main.c
+++ freeRTOS Project/Core/Src/main.c
@@ -21,7 +21,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "FreeRTOS.h"
+#include "task.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -49,7 +50,8 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void task1_handler(void* parameters);
+static void task2_handler(void* parameters);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -65,7 +67,9 @@ int main(void)
 {
 
   /* USER CODE BEGIN 1 */
-
+	TaskHandle_t task1_handle;
+	TaskHandle_t task2_handle;
+	BaseType_t status;
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -87,7 +91,13 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
+  status = xTaskCreate(task1_handler, "Task 1", 200, "from Task1", 2, task1_handle);
+  configASSERT(status == pdPASS);
 
+  status = xTaskCreate(task2_handler, "Task 2", 200, "from Task2", 2, task2_handle);
+  configASSERT(status == pdPASS);
+
+  vTaskStartScheduler();
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -167,7 +177,20 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+static void task1_handler(void * parameters){
 
+	for (;;) {
+
+	}
+
+}
+static void task2_handler(void * parameters){
+
+	for (;;) {
+
+	}
+
+}
 /* USER CODE END 4 */
 
 /**
--- freeRTOS Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h
+++ freeRTOS Project/ThirdParty/FreeRTOS/FreeRTOSConfig.h
@@ -90,10 +90,10 @@
 
 //#include "system_stm32 f4xx.h"
 
-#define configSUPPORT_STATIC_ALLOCATION 1
+#define configSUPPORT_STATIC_ALLOCATION 0
 
 #define configUSE_PREEMPTION   1
-#define configUSE_IDLE_HOOK    1
+#define configUSE_IDLE_HOOK    0
 #define configUSE_TICK_HOOK    0
 #define configCPU_CLOCK_HZ    ( SystemCoreClock )
 #define configTICK_RATE_HZ    ( ( TickType_t ) 1000 )
